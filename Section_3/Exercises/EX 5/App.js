0c1d73c379ab343f2622ffdbcf02cdfb5c8e56da
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-08-24 15:20:01 +0300

    Ex 5 is Done

---
+++ Section_3/Exercises/EX 5/App.js
@@ -0,0 +1,30 @@
+export function CourseGoal({title,description}) {
+    return (
+      <li>
+        <h2>{title}</h2>
+          <p>{description}</p>
+      </li>
+    );
+  }
+  
+  function App() {
+    return (
+      <div id="app" data-testid="app">
+        <h1>Time to Practice</h1>
+        <p>One course, many goals! 🎯</p>
+        <ul>
+        <CourseGoal title='Learn React'
+        description="In-depth" />
+        <CourseGoal 
+        title='Learn React2'
+        description="In-depth2"
+        />
+        
+          {/* OUTPUT AT LEAST TWO CourseGoal components here */}
+          {/* One of them should have a title of “Learn React” and a description of “In-depth” */}
+        </ul>
+      </div>
+    );
+  }
+  
+  export default App;

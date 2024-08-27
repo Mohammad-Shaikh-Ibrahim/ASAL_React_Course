9da522d70ceef3fdaa2e3cef2dd0ea6a5c5faa80
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-08-27 22:24:10 +0300

    Deriving State From Props

--- Section_4/Tic-Tac-Toe_Project/src/App.jsx
+++ Section_4/Tic-Tac-Toe_Project/src/App.jsx
@@ -1,15 +1,40 @@
+import { useState } from "react"
 import Player from "./components/Player"
+import GameBoard from "./components/GameBoard"
+import Log from "./components/Log";
+
 function App() {
-  
+  const [activePlayer,setActivePlayer]= useState('X');
+  const [gameTurns,setGameTurns]= useState([]);
+
+  function handelSelectedSquare(rowIndex,colIndex){
+    setActivePlayer((curActivePlayer)=>curActivePlayer === 'X'?'O':'X')
+    setGameTurns(prevTurns => {
+      let currentPlayer= 'X';
+
+      if(prevTurns.length>0 && prevTurns[0].player ==='X'){
+        currentPlayer='O';
+      }
+
+      const updateTurns = [
+        {square: { row: rowIndex, col: colIndex }, player:currentPlayer},
+        ...prevTurns,];
+
+        return updateTurns;
+      });
+  }
 
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player1" symbol="X" />
-          <Player initialName="Player2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player1" symbol="X" isActive={activePlayer === 'X'}/>
+          <Player initialName="Player2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
+        <GameBoard onSelectSquare={handelSelectedSquare} 
+        turns={gameTurns}/>
       </div>
+      <Log/>
     </main>
   )
 }
---
+++ Section_4/Tic-Tac-Toe_Project/src/components/GameBoard.jsx
@@ -0,0 +1,51 @@
+// import { useState } from "react";
+const initialGameBoard =[
+    [null,null,null],
+    [null,null,null],
+    [null,null,null]
+]
+
+
+
+export default function GameBoard({onSelectSquare,turns}){
+
+    let gameBoard = initialGameBoard;
+
+    for(const turn of turns){
+        const {square,player} = turn;
+        const {row,col}= square;
+
+        gameBoard[row][col]= player;
+    }
+
+    // const [gameBoard,setGameBoard]= useState(initialGameBoard);
+
+    // function handelSelectedSquare(rowIndex,colIndex){
+    //     setGameBoard(prevGameBoard =>{
+    //         const updateGameBoard= [...prevGameBoard.map((innerArray)=>[...innerArray])];
+    //         updateGameBoard[rowIndex][colIndex]= activePlayerSymbol;
+    //         return updateGameBoard
+    //     })
+    //     onSelectSquare();
+    // }
+
+    return(
+        <ol id="game-board">
+            {gameBoard.map((row,rowIndex)=>(
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol,colIndex)=>(
+                            <li key={colIndex}>
+                                <button onClick={ ()=> onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                                    </li>
+                                )
+                            )
+                        }
+                            </ol>
+                        </li>
+                    )
+                )
+            }
+        </ol>
+    );
+}

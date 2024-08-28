b6b8cc0517b9880d4bcce41ac8ad2864bad5e11e
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-08-28 16:36:35 +0300

    Finish The Project and Outputting Content Conditionally

--- Section_5/Web_Calculator/src/App.jsx
+++ Section_5/Web_Calculator/src/App.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
+
 import Header from "./components/Header"
 import UserInput from "./components/UserInput"
+import Results from "./components/Results"
 function App() {
+
+  const [userInput,setUserInput]= useState({
+    initialInvestment: 15000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+});
+
+const inputIsValid= userInput.duration >=1;
+
+function handelChange(inputIdentifier,newValue){
+    setUserInput(prevUserInput => {
+        return{...prevUserInput,[inputIdentifier]:+newValue}});
+}
+
+
+
   return (
     <>
     <Header/>
-    <UserInput/>
+    <UserInput userInput={userInput} onChange={handelChange}/>
+    {!inputIsValid && <p className="center">Please Enter a Duration Greater Than Zero</p>}
+    {inputIsValid && <Results input={userInput}/>}
     </>
   )
 }
--- Section_5/Web_Calculator/src/components/UserInput.jsx
+++ Section_5/Web_Calculator/src/components/UserInput.jsx
@@ -1,22 +1,19 @@
-import { useState } from "react"
+// import { useState } from "react"
 
 
+export default function UserInput({onChange,userInput}){
+    /*cut into App.jsx */
+    // const [userInput,setUserInput]= useState({
+    //     initialInvestment: 1000,
+    //     annualInvestment: 1200,
+    //     expectedReturn: 6,
+    //     duration: 10
+    // });
 
-
-
-
-
-export default function UserInput(){
-    const [userInput,setUserInput]= useState({
-        initialInvestment: 1000,
-        annualInvestment: 1200,
-        expectedReturn: 6,
-        duration: 10
-    });
-
-    function handelChange(inputIdentifier,newValue){
-        setUserInput(prevUserInput => {return{...prevUserInput,[inputIdentifier]:newValue}});
-    }
+    // function handelChange(inputIdentifier,newValue){
+    //     setUserInput(prevUserInput => {
+    //         return{...prevUserInput,[inputIdentifier]:newValue}});
+    // }
 
     return <section id="user-input">
         <div className="input-group">
@@ -26,7 +23,7 @@ export default function UserInput(){
                 type="number"
                 required 
                 value={userInput.initialInvestment} 
-                onChange={(event)=> handelChange('initialInvestment',event.target.value)}
+                onChange={(event)=> onChange('initialInvestment',event.target.value)}
                 />
             </p>
             <p>
@@ -35,7 +32,7 @@ export default function UserInput(){
                 type="number" 
                 required
                 value={userInput.annualInvestment}
-                onChange={(event)=> handelChange('annualInvestment',event.target.value)}
+                onChange={(event)=> onChange('annualInvestment',event.target.value)}
                 />
             </p>
         </div>    
@@ -46,7 +43,7 @@ export default function UserInput(){
                 type="number" 
                 required
                 value={userInput.expectedReturn}
-                onChange={(event)=> handelChange('expectedReturn',event.target.value)}
+                onChange={(event)=> onChange('expectedReturn',event.target.value)}
                 />
             </p>
             <p>
@@ -55,7 +52,7 @@ export default function UserInput(){
                 type="number" 
                 required
                 value={userInput.duration}
-                onChange={(event)=> handelChange('duration',event.target.value)}
+                onChange={(event)=> onChange('duration',event.target.value)}
                 />
             </p>
         </div>

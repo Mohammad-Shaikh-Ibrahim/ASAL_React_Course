29aad54961502f3ab39648ae5eacae6d8c3e8d75
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-09-01 23:31:17 +0300

    Manipulating the DOM via Refs

--- Section_8/Main/src/components/Player.jsx
+++ Section_8/Main/src/components/Player.jsx
@@ -1,10 +1,34 @@
+import {useState,useRef} from 'react'
+
 export default function Player() {
+  const playerName =useRef();
+  const [enteredPlayerName,setEnteredPlayerName]=useState(null);
+  // const [submitted,setSubmitted]=useState(false);
+
+  // function handelChange(event){
+  //   setSubmitted(false);
+  //   setEnteredPlayerName(event.target.value);
+  // }
+
+  function handelClick(){
+    setEnteredPlayerName(playerName.current.value); // to save the currunt name while typing
+    playerName.current.value= ''; // to clean entry after click button
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}
+         {/* {submitted? enteredPlayerName: 'unknown entity'} */}
+        {/*??=> {enteredPlayerName? enteredPlayerName: 'unknown entity'} */}
+        </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input 
+        ref={playerName} 
+        type="text" 
+        // onChange={handelChange} 
+        // value={enteredPlayerName}
+        />
+        <button onClick={handelClick}>Set Name</button>
       </p>
     </section>
   );

7185a7d1caa21c05b80a7308bcd48b2558d05aee
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-09-02 14:06:07 +0300

    Introducing & Understanding "Portals"

--- Section_8/Main/src/components/ResultModal.jsx
+++ Section_8/Main/src/components/ResultModal.jsx
@@ -1,5 +1,5 @@
 import { forwardRef , useImperativeHandle,useRef} from "react";
-
+import { createPortal } from "react-dom";
  const ResultModal= forwardRef(function ResultModal({targetTime,remainingTime,onReset},ref){
     const dialog= useRef()
 
@@ -14,7 +14,7 @@ import { forwardRef , useImperativeHandle,useRef} from "react";
         }
     })
 
-    return (
+    return createPortal(
     <dialog ref={dialog} className="result-modal" onClose={onReset}>
         {userLost && <h2>You Lost</h2>}
         {!userLost && <h2>Your Score:{score}</h2>}
@@ -23,7 +23,8 @@ import { forwardRef , useImperativeHandle,useRef} from "react";
         <form method="dialog" onSubmit={onReset}>
             <button>Close</button>
         </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('model')
     );
 })
 

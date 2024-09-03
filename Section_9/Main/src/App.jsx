06d9a097c26d074dfa77d93bf044672dca73da9f
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-09-03 14:33:38 +0300

    Adding a "Projects Sidebar" Component

--- Section_9/Main/src/App.jsx
+++ Section_9/Main/src/App.jsx
@@ -1,7 +1,8 @@
+import ProjectsSidebar from "./components/ProjectsSidebar";
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+     <ProjectsSidebar/>
     </>
   );
 }

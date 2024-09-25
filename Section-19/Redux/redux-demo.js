f6e130ad1a566e0c76842e7d62cd26332b4266be
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-09-25 17:13:39 +0300

    Exploring The Core Redux Concepts

---
+++ Section-19/Redux/redux-demo.js
@@ -0,0 +1,21 @@
+const redux = require("redux");
+
+//Reducer Function
+const counterReducer = (state = { counter: 0 }, action) => {
+  return {
+    counter: state.counter + 1,
+  };
+};
+
+const store = redux.createStore(counterReducer);
+
+// console.log(store.getState());
+
+const counterSubscriber = () => {
+  const latesState = store.getState();
+  console.log(latesState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });

d3877f56d856491a5f72fbcfeb952ba8cb90f3dd
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-09-26 10:33:59 +0300

    More Redux Basics

--- Section-19/Redux/redux-demo.js
+++ Section-19/Redux/redux-demo.js
@@ -2,9 +2,17 @@ const redux = require("redux");
 
 //Reducer Function
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
+  if(action.type === "increment"){
+    return {
     counter: state.counter + 1,
-  };
+    };
+  }
+  if(action.type === "decrement"){
+    return {
+    counter: state.counter - 1,
+    };
+  }
+  return state;
 };
 
 const store = redux.createStore(counterReducer);
@@ -19,3 +27,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });

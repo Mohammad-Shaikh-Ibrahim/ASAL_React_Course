6a3e70f9446865aca481819b86050ca2732daea8
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-09-26 17:04:05 +0300

    Reading & Dispatching From A New Slice

--- Section-19/Redux/Redux-Counter/src/App.jsx
+++ Section-19/Redux/Redux-Counter/src/App.jsx
@@ -1,13 +1,20 @@
+import { useSelector } from 'react-redux';
+
 import Header from './components/Header.jsx'
 import Counter from './components/Counter.jsx';
 import Auth from './components/Auth.jsx'
+import UserProfile from './components/UserProfile.jsx'
 
 function App() {
+
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
   return (
     <>
-    <Header/>
-    <Auth/>
-    <Counter />
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
     </>
   );
 }

0f21e31e7eb51e8a3457575b5f7a6fe46cc81c30
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-09-26 16:15:24 +0300

    Connecting Redux Toolkit State

--- Section-19/Redux/Redux-Counter/src/store/index.jsx
+++ Section-19/Redux/Redux-Counter/src/store/index.jsx
@@ -1,9 +1,10 @@
-import { createStore } from "redux";
-import { createSlice } from "@reduxjs/toolkit";
+//here we replace createStore by configureStore because at lastet version doesnt use
+// import { createStore } from "redux";
+import { createSlice,configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-createSlice({
+const counterSlice=createSlice({
     name: 'counter',
     initialState, //=>initialState: initialState
     reducers: {
@@ -25,49 +26,51 @@ createSlice({
     }
 });
 
-const counterReducer = (state = initialState, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-        };
-        //don't do this:
-        // state.counter++
-        // return{
-        //     counter: state.counter,
-        //     showCounter: state.showCounter
-        // }
-        // or you forget the showCounter: state.showCounter at every return 
-        // ** So always Copy and Create New Objects Dont Forget any thing
-        // vist to this link https://academind.com/tutorials/reference-vs-primitive-values/
-    }
-    if (action.type === 'increase') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
-        };
-    }
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        }
-    }
-    if (action.type === 'decrease') {
-        return {
-            counter: state.counter - action.amount,
-            showCounter: state.showCounter
-        };
-    }
-    if (action.type === 'toggle') {
-        return {
-            showCounter: !state.showCounter,
-            counter: state.counter
-        }
-    }
-    return state;
-}
+// const counterReducer = (state = initialState, action) => {
+//     if (action.type === 'increment') {
+//         return {
+//             counter: state.counter + 1,
+//             showCounter: state.showCounter
+//         };
+//         //don't do this:
+//         // state.counter++
+//         // return{
+//         //     counter: state.counter,
+//         //     showCounter: state.showCounter
+//         // }
+//         // or you forget the showCounter: state.showCounter at every return 
+//         // ** So always Copy and Create New Objects Dont Forget any thing
+//         // vist to this link https://academind.com/tutorials/reference-vs-primitive-values/
+//     }
+//     if (action.type === 'increase') {
+//         return {
+//             counter: state.counter + action.amount,
+//             showCounter: state.showCounter
+//         };
+//     }
+//     if (action.type === 'decrement') {
+//         return {
+//             counter: state.counter - 1,
+//             showCounter: state.showCounter
+//         }
+//     }
+//     if (action.type === 'decrease') {
+//         return {
+//             counter: state.counter - action.amount,
+//             showCounter: state.showCounter
+//         };
+//     }
+//     if (action.type === 'toggle') {
+//         return {
+//             showCounter: !state.showCounter,
+//             counter: state.counter
+//         }
+//     }
+//     return state;
+// }
 
-const store = createStore(counterReducer);
-
+// const store = createStore(counterReducer);
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
 export default store;

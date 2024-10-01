0e06ca7c6700d115fa2a2e2fe3051a2b9bcc1383
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-10-01 16:43:11 +0300

    Validating Input via Built-in Validation Props

--- Section-17/Main/src/App.jsx
+++ Section-17/Main/src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './components/Header.jsx';
 // import StateLogin from './components/StateLogin.jsx';
-import Login from './components/Login.jsx';
-// import Signup from './components/Signup.jsx';
+// import Login from './components/Login.jsx';
+import Signup from './components/Signup.jsx';
 
 function App() {
   return (
@@ -9,8 +9,8 @@ function App() {
       <Header />
       <main>
         {/* <StateLogin/> */}
-        <Login />
-        {/* <Signup/> */}
+        {/* <Login /> */}
+        <Signup/>
       </main>
     </>
   );
